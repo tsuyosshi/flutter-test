0ddb7b673822003e9e3f7d62d6b912db15aae135
s55224kt <koutarou1276@gmail.com>
2023-03-17 15:12:02 +0900

    edit frontend

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"goapp/data"
+	"log"
 	"net/http"
 )
+
+type User struct {
+	Id   int
+	Name string
+}
 
 func main() {
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/login", testLogin)
+	http.HandleFunc("/get", getHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -18,4 +26,16 @@ func hello(w http.ResponseWriter, r *http.Request) {
 func testLogin(w http.ResponseWriter, r *http.Request) {
 	_, user_name := data.TestPost()
 	w.Write([]byte(user_name))
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	id, user_name := data.TestPost()
+	user := User{id, user_name}
+	res, err := json.Marshal(user)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
 }
--- frontend/app/lib/main.dart
+++ frontend/app/lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
+}
+
+class User {
+  int id;
+  String name;
+
+  User({required this.id, required this.name});
+
+  factory User.fromJson(Map<String, dynamic> json) =>
+      User(id: json["Id"], name: json["Name"]);
+
+  Map<String, dynamic> toJson() => {"Id": id, "Name": name};
 }
 
 class MyApp extends StatelessWidget {
@@ -50,15 +66,35 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  User user = User(id: -1, name: 'hoge');
+
+  Future<User?> getUser() async {
+    final response = await http.get(Uri.parse('http://192.168.11.34:8080/get'));
+    try {
+      if (response.statusCode == 200) {
+        final body = jsonDecode(response.body);
+        return User(id: body['Id'], name: body['Name']);
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+    return null;
+  }
+
+  void setUser() async {
+    // final url = Uri.parse('https://api-ninjas.com/api/quotes');
+    // final response = await http.get(url);
+    final _user = (await getUser())!;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      user = _user;
     });
+  }
+
+  @override
+  void initState() {
+    //アプリ起動時に一度だけ実行される
   }
 
   @override
@@ -69,47 +105,42 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    setUser();
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button to get user infomation:',
+              ),
+              Text(
+                user.name,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
+        ));
   }
 }
